3e8f4fbac39ad573d205af45c1cce8fbb9502497
dir-FY24takahashiyuuki <yuki.takahashi.b138@dirbato.co.jp>
2024-04-25 11:06:52 +0900

    Lesson 21Chapter 5.16

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set; // 追加
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,5 +31,12 @@ public class UserService {
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 }
